3f5f2d6b8fa064a4131037dec6d79c1474f47532
tsumugu <tsumugu1515@gmail.com>
2021-03-28 21:55:40 +0900

    Initial Commit

---
+++ LowBatteryAlert2/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LowBatteryAlert2
+//
+//  Created by 山口紡 on 2021/03/28.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LowBatteryAlert2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LowBatteryAlert2
+//
+//  Created by 山口紡 on 2021/03/28.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
